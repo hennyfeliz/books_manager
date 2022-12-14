6ea5a448b59e3db0c08c0d17916cf8ad39c74db4
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-12-13 23:30:14 -0400

    Cambios de estilo en la pantalla principal

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <p>funciona</p>
+      <div className="App mt-3 border 1px solid red">
+        <Container>
+          <p className="m-auto p-1">Books manager</p>
+          <p>-{">"} esto se va a quitar xd</p>
+        </Container>
+      </div>
+      <Container>
+        <Button className="mt-5 w-75">Ir al menu</Button>
+      </Container>
     </div>
   );
 }
