631e24bf4da2983f0ff5e7c43e460f23e826a5a6
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-12-14 19:52:21 -0400

    Agregando footer y navbar components al proyecto

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button } from "react-bootstrap";
+import { NavBarComponent } from "./Components/NavBar/NarBarComponent";
+import { FooterComponent } from "./Components/Footer/FooterComponent";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App mt-3 border 1px solid red">
+    <>
+      <NavBarComponent />
+      <div className="App">
+        <div className="App mt-3 border 1px solid red">
+          <Container>
+            <p className="m-auto p-1">Books manager</p>
+            <p>-{">"} esto se va a quitar xd</p>
+          </Container>
+        </div>
         <Container>
-          <p className="m-auto p-1">Books manager</p>
-          <p>-{">"} esto se va a quitar xd</p>
+          <Button className="mt-5 w-75">Ir al menu</Button>
         </Container>
       </div>
-      <Container>
-        <Button className="mt-5 w-75">Ir al menu</Button>
-      </Container>
-    </div>
+      <FooterComponent />
+    </>
   );
 }
 
---
+++ FRONTEND/src/Components/Footer/FooterComponent.jsx
@@ -0,0 +1,17 @@
+export const FooterComponent = () => {
+  return (
+    <div className="footer_component mt-5 navbar_font">
+      <footer className="bg-dark text-center text-white">
+        <div
+          className="text-center p-3 mt-4"
+          //style="background-color: rgba(0, 0, 0, 0.2);"
+        >
+          © 2022 Copyright:
+        <p>
+          Henny Feliz - Carlos Ramirez
+        </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
