2a31311c4d587aee75351addfdc09ea4e1605275
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-12-18 14:42:41 -0400

    Agregando mas cambios

--- FRONTEND/src/Components/SideBar/SideBarComponent.jsx
+++ FRONTEND/src/Components/SideBar/SideBarComponent.jsx
@@ -1,5 +1,6 @@
-import './SideBarStyles.css'
-import { sideBarElements } from './SideBarElementsTest';
+import "./SideBarStyles.css";
+import { sideBarElements } from "./SideBarElementsTest";
+import { Link } from "react-router-dom";
 
 export const SideBarComponent = () => {
   return (
@@ -10,18 +11,21 @@ export const SideBarComponent = () => {
       >
         <div class="position-sticky">
           <div class="list-group list-group-flush mx-3 mt-4">
-          <a
-              href="#"
-              class="list-group-item list-group-item-action py-2 ripple"
-              aria-current="true"
-            >
-              {/* <i class="fas fa-tachometer-alt fa-fw me-3"></i> */}
-              <span>Main dashboard</span>
-            </a>
+            {sideBarElements.map((element) => {
+              return (
+                <a
+                  to={""}
+                  className="sidebar_font list-group-item list-group-item-action py-2 ripple"
+                  aria-current="true"
+                >
+                  {/* <i class="fas fa-tachometer-alt fa-fw me-3"></i> */}
+                  <span>{element.text}</span>
+                </a>
+              );
+            })}
           </div>
         </div>
       </nav>
-      
     </>
   );
 };
@@ -116,9 +120,6 @@ const anotherSideBarComponent = () => {
           </div>
         </div>
       </nav>
-      
     </>
   );
-}
-
-
+};
