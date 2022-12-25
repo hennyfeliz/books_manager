e8e582dbabd4e52f38eb1850fd188c25040e9b91
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-12-24 20:07:11 -0400

    Agregando metodos para fetching de datos

---
+++ FRONTEND/src/Components/Helpers/AxiosHelper.js
@@ -0,0 +1,54 @@
+import axios from "axios"
+
+
+export const AxiosFetchGET = () => {
+    axios.get(``)
+    .then((response) => {
+
+    })
+    .catch((response) => {
+
+    })
+}
+
+export const AxiosFetchPOST = () => {
+  axios.post(``)
+  .then((response) => {
+
+  })
+  .catch((response) => {
+
+  })
+}
+
+export const AxiosFetchPUT = () => {
+  axios.put(``)
+  .then((response) => {
+
+  })
+  .catch((response) => {
+
+  })
+}
+
+export const AxiosFetchDELETE = () => {
+  axios.delete(``)
+  .then((response) => {
+
+  })
+  .catch((response) => {
+
+  })
+}
+
+export const AxiosFetchPATCH = () => {
+  axios.patch(``)
+  .then((response) => {
+
+  })
+  .catch((response) => {
+
+  })
+}
+
+
