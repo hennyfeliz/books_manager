3dbd854caab68ddd5e2c62cfa17512834220550c
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-01-02 18:29:35 -0400

    Cambios modal component

--- FRONTEND/src/Components/CardComponent/ElementCard.jsx
+++ FRONTEND/src/Components/CardComponent/ElementCard.jsx
@@ -4,21 +4,13 @@ import { useState } from "react";
 import { ModalComponentImformation } from "../ModalComponent/ModalComponent";
 
 export const Element = ({ element }) => {
-  const [show, setShow] = useState(false);
   const [info, setInfo] = useState(false);
-  const [edit, setEdit] = useState(false);
-
-  const editClose = () => setEdit(false);
-  const editShow = () => setEdit(true);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const infoClose = () => setInfo(false);
   const infoShow = () => setInfo(true);
 
   return (
-    <div className="product m-4">
+    <div className="product m-4" onClick={infoShow} >
       <img
         src="https://picsum.photos/180"
         alt="products"
@@ -28,12 +20,12 @@ export const Element = ({ element }) => {
       <div className="product_details">
         {/* <p className="product_oldprice">300</p> */}
         <p className="product_newprice">${element.precio} US</p>
-        <Button onClick={handleShow} />
+        <Button/>
       </div>
       <ModalComponentImformation
         info={info}
         infoClose={infoClose}
-        infoName="Curso"
+        element={element}
       />
     </div>
   );
--- FRONTEND/src/Components/ModalComponent/ModalComponent.jsx
+++ FRONTEND/src/Components/ModalComponent/ModalComponent.jsx
@@ -1,27 +1,34 @@
 import { Button, Modal } from "react-bootstrap";
 import { useState } from "react";
+import "./ModalStyles.css"
+
 
 export const ModalComponentImformation = ({
   info,
   infoClose,
-  message,
-  infoName,
+  element
 }) => {
   return (
-    <>
+    <div className="modal_styles">
       <Modal show={info} onHide={infoClose}>
         <Modal.Header closeButton>
-          <Modal.Title className="display-6">{`Informacion de ${infoName}`}</Modal.Title>
+          <Modal.Title className="display-6">{`Titulo: ${element.titulo}`}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h1>nada</h1>
+          <p>{element.descripcion}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={infoClose}>
+          <p>${element.precio} US</p>
+        </Modal.Footer>
+        <Modal.Footer>
+          <Button className="p-1 h-25" variant="secondary" onClick={infoClose}>
             Cerrar
+          </Button>
+          <Button className="w-50 h-25 p-1">
+            Agregar al carrito
           </Button>
         </Modal.Footer>
       </Modal>
-    </>
+    </div>
   );
 };
