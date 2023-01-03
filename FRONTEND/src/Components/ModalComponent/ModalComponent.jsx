a3e20a65fd7c974b37c66c7db91f93710c5ed81f
Henny M. Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-01-02 20:35:56 -0400

    Cambios

--- FRONTEND/src/Components/ModalComponent/ModalComponent.jsx
+++ FRONTEND/src/Components/ModalComponent/ModalComponent.jsx
@@ -12,19 +12,19 @@ export const ModalComponentImformation = ({
     <div className="modal_styles">
       <Modal show={info} onHide={infoClose}>
         <Modal.Header closeButton>
-          <Modal.Title className="display-6">{`Titulo: ${element.titulo}`}</Modal.Title>
+          <Modal.Title className="display-6 modal_styles">{`Titulo: ${element.titulo}`}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>{element.descripcion}</p>
+          <p className="modal_styles">{element.descripcion}</p>
         </Modal.Body>
         <Modal.Footer>
-          <p>${element.precio} US</p>
+          <p className="modal_styles">${element.precio} US</p>
         </Modal.Footer>
         <Modal.Footer>
-          <Button className="p-1 h-25" variant="secondary" onClick={infoClose}>
+          <Button className="p-1 h-25 modal_styles" variant="secondary" onClick={infoClose}>
             Cerrar
           </Button>
-          <Button className="w-50 h-25 p-1">
+          <Button className="w-50 h-25 p-1 modal_styles">
             Agregar al carrito
           </Button>
         </Modal.Footer>
